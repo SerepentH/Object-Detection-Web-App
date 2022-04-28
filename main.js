89e9b8cca912fccb9004b5b21faa4c05e66a01bd
SerepentH <79256227+SerepentH@users.noreply.github.com>
2022-04-28 13:09:33 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+img = " ";
+
+function preload()
+{
+    img = loadImage('dog_cat.jpg');
+}
+
+function setup()
+{
+    canvas = createCanvas(640, 370);
+    canvas.position(320, 150);
+}
+
+function draw()
+{
+    image(img, 0, 0, 640, 370);
+    fill("#FF0000");
+    text("Dog", 45, 75);
+    noFill();
+    stroke("#FF0000");
+    rect(30, 60, 450, 300);   
+}
